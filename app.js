1905dae258079455b12fb69fccb3e98a561e2ddf
andriikryshtal <andrewk@airbeem.com>
2018-06-08 20:49:40 +0300

    LUIS setup

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import path from "path";
 import bodyParser from "body-parser";
 import request from "request";
 import { MongoClient } from "mongodb";
-// const db = require("./db");
 import { connect } from "./db";
+// import { luisIntent } from "./luis";
 import config from "./config";
+import querystring from "query-string";
 
 var app = express();
 let token =
@@ -12,8 +14,11 @@ let token =
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("938470555");
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -29,17 +34,41 @@ app.get("/webhook", (req, res) => {
 });
 
 function sendText(sender, text) {
-  console.log("text: " + text);
-  console.log("sender: " + sender);
+  // console.log("text: " + text);
+  // console.log("sender: " + sender);
+  getLuisIntent(text);
   request(
     {
       url: "https://graph.facebook.com/v2.6/me/messages?access_token=" + token,
       method: "POST",
       headers: { "Content-Type": "application/json; charset=UTF-8" },
       json: {
-        messaging_type: "RESPONSE",
-        recipient: { id: sender },
-        message: { text: text }
+        recipient: {
+          id: sender
+        },
+        message: {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "generic",
+              elements: [
+                {
+                  title: "Welcome!",
+                  image_url:
+                    "https://d1ro8r1rbfn3jf.cloudfront.net/ms_61397/LamNf1Y6jSk5uB5pSSiSfbCInh0rFk/pic.jpg?Expires=1528469721&Signature=n8LDHnkAKKcbpnuXF3edNq5XRXNlSvkYQ3U2u2MB5hRl4pjWF4fuhAx0qllu1xLpM8BmJGYS-pkhYllalWZviO7esT5ZQteaBxxPmfxYz5GUMaD-uWg0Q9yeTu2Qm6NWuMfCtTE3Gmbv40LZSAHQ7w1th-BZOynP~tdUvxTWFaqBaKnqODtExDH-8y77TqTzEci6Du61Hu28b9poROwaZlth6IUqrBVxEbGRoKM4aCVSUorD6loyD3LNNdN2LiBLTgPuszT7bq4Dvhd1rJ0R0TcLbaL6~FjK4kMnB9GBp39lU2Lkjrb6-Vum4MAZQuM4G3saeR2eZDY1wYzOtBazSQ__&Key-Pair-Id=APKAJHEJJBIZWFB73RSA",
+                  subtitle: "We have the right hat for everyone."
+                  // buttons: [
+                  //   {
+                  //     type: "postback",
+                  //     title: "Start Chatting",
+                  //     payload: "test"
+                  //   }
+                  // ]
+                }
+              ]
+            }
+          }
+        }
       }
     },
     (error, response, body) => {
@@ -52,14 +81,47 @@ function sendText(sender, text) {
   );
 }
 
+function getLuisIntent(utterance) {
+  var endpoint = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
+  var luisAppId = "7ff73086-9a62-4e1b-ad7c-3f64fac48aaf";
+  var queryParams = {
+    "subscription-key": "0434d0a9bb834d3f9358a37ddd9e9708",
+    timezoneOffset: "1",
+    verbose: true,
+    q: utterance
+  };
+
+  var luisRequest =
+    endpoint + luisAppId + "?" + querystring.stringify(queryParams);
+
+  return new Promise(function(resolve, reject) {
+    request(luisRequest, function(err, response, body) {
+      if (err) console.log(err);
+      else {
+        var data = JSON.parse(body);
+
+        resolve(JSON.stringify(data));
+      }
+    });
+  });
+}
+
 app.post("/webhook", (req, res) => {
   let messaging = req.body.entry[0].messaging;
-  console.log("sender: " + req.body.entry[0].messaging[0].sender.id);
+  // console.log("sender: " + req.body.entry[0].messaging[0].sender.id);
   for (let i = 0; i < messaging.length; i++) {
     let sender = messaging[i].sender.id;
     if (messaging[i].message && messaging[i].message.text) {
       let text = messaging[i].message.text;
-      sendText(sender, "text echo: " + text.substring(0, 100));
+      getLuisIntent(text).then(function(data) {
+        var currentDate = new Date();
+        console.log(currentDate);
+        console.log(JSON.stringify(data));
+        // sendText(sender, data);
+      });
+    }
+    if (messaging[0].postback && messaging[0].postback.payload === "test") {
+      console.log("postback: " + messaging[0].postback.payload);
     }
   }
   res.sendStatus(200);
